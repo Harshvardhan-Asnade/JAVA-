c47ad64426bacd37ab9f6c94c763de32a11551d6
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-07-02 16:25:04 +0530

    Datatype

---
+++ Datatype.java
@@ -0,0 +1,10 @@
+public class Datatype {
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 100;
+    System.out.println(a);
+    System.out.println(b);
+    String name = "Iron man";
+    System.out.print(name);
+  }
+}
