062070e0dc15a9b2824799d88d9b5a8a8fdc7d3a
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-07-10 18:58:14 +0530

    Product

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+   Scanner Product =new Scanner(System.in);
+   System.out.println("Enter the value of A");
+   int A = Product.nextInt();
+     System.out.println("Enter the value of B");
+   int B = Product.nextInt();
+   int PRoDucto = A*B ;
+   System.out.println(PRoDucto);
+   
+   
+  }
+}
