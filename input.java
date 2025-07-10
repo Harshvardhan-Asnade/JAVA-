5ab3183e4327bca78555859081ca8eea2b89c686
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-07-10 18:38:53 +0530

    input

---
+++ input.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Input{
+  public static void main (String[] args) {
+    Scanner sc = new Scanner(System.in);
+    // String input =sc.next();
+    // System.out.println(input);
+    // int a = sc.nextInt();
+    // System.out.println(a);
+    // float f = sc.nextFloat();
+    // System.out.println(f);
+    boolean x = sc.nextBoolean();
+    System.out.println(x);
+  
+    
+    long longValue = sc.nextLong();
+    System.out.println(  longValue);
+ 
+     
+    short shortValue = sc.nextShort();
+    System.out.println( shortValue);
+
+   
+    byte byteValue = sc.nextByte();
+    System.out.println( byteValue);
+
+  }
+}
