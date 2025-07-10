68c065d05e71f640d5a62339482d4ea24d2823d0
Harshvardhan-Asnade <harshasnade1@gmail.com>
2025-07-10 19:11:02 +0530

    Area

---
+++ area.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class area {
+    public static void main(String[] args) {
+   Scanner Product =new Scanner(System.in);
+   System.out.println("Enter radius");
+   int r  = Product.nextInt();
+      
+  Double Area = 3.14 * r  * r  ;
+   System.out.println(Area);
+   
+   
+  }
+}
